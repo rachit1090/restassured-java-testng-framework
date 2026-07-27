2124d7834d7024a3d87fd104b0aaf674b4dc2f9f
rachit1090 <rachit1090@gmail.com>
2026-07-27 19:31:43 +0530

    Update EndPoint class with DELETE_BOOK and UPDATE_BOOK endpoints

--- src/main/java/constants/EndPoint.java
+++ src/main/java/constants/EndPoint.java
@@ -5,6 +5,7 @@ public class EndPoint
 	private EndPoint() {}
 	
 	public static final String ADD_BOOK = "/Library/Addbook.php";
-	public static final String GET_BOOK	= "/Library/GetBook.php";
-	public static final String DEL_BOOK = "/Library/DeleteBook.php";		
+	public static final String GET_BOOK = "/Library/GetBook.php";
+	public static final String DELETE_BOOK = "/Library/DeleteBook.php";
+	public static final String UPDATE_BOOK = "/Library/UpdateBook.php";
 }
